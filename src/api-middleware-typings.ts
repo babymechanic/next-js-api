f1adfa9badaffefd34e9739c872d02ced45f7b68
piggy <babymechanic@gmail.com>
2022-05-15 20:12:35 +1000

    fixing typings

--- src/api-middleware-typings.ts
+++ src/api-middleware-typings.ts
@@ -1,7 +1,7 @@
 import { PerRequestContext } from './per-request-context';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export type ApiRouteHandler = (req: NextApiRequest, res: NextApiResponse, context: PerRequestContext) => Promise<unknown>;
+export type ApiRouteHandler = (req: NextApiRequest, res: NextApiResponse, context: PerRequestContext) => unknown;
 export type ApiRouteMiddleware = ApiRouteHandler;
 
 export type ApiRouteMethods = 'post' | 'get' | 'patch' | 'delete' | 'options';
